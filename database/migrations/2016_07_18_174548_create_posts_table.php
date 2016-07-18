000cd2258236a2f56673f95fda2a239cb55a514b
Franklin Rodriguez <franklingra@outlook.com>
2016-07-18 14:03:05 -0400

    Model and migrations for Post and Comments.

---
+++ database/migrations/2016_07_18_174548_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->date('date');
+            $table->int('autor_id');
+            $table->string('tags');
+            $table->text('content');
+            $table->int('commments_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
